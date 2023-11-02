e2ead0fbae034e474bbf512e2f9b939bd91395f8
khalid <khalid.dinh@gmail.com>
2023-11-02 11:55:17 +0700

    add request file, update code

--- src/main/java/com/example/accessingdatamysql/MainController.java
+++ src/main/java/com/example/accessingdatamysql/MainController.java
@@ -29,7 +29,7 @@ public class MainController {
     return "Saved";
   }
 
-  @GetMapping(path = "/all2")
+  @GetMapping(path = "/all")
   public @ResponseBody Iterable<User> getAllUsers() {
     // This returns a JSON or XML with the users
     return userRepository.findAll();
